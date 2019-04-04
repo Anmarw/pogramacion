424274a0cfba1f763133fec1d4643a43e919b14c
pavel <adrianpavel@gmail.com>
2019-04-04 09:10:18 +0200

    Dado.java

--- Dado.java
+++ Dado.java
@@ -1,6 +1,16 @@
-class Dado {
-private int valor;
-public void tirarDado(){
- 
-	}
+
+public class Dado {
+	private int valor;
+
+		public Dado(){
+			
+		}
+		public void tirarDado(){
+			this.valor = (int)((Math.random()*6)+1);
+			return this.valor;
+		}
+		
+		public int getValor(){
+			return this.valor;
+		}
 }
